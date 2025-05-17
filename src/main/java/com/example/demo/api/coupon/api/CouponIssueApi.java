b29e8c518769bc87412011bea193554ec6a756e7
sukkyun2 <goobghd@gmail.com>
2025-05-17 14:23:46 +0900

    feat: NoQuantityException 예외 반영

--- src/main/java/com/example/demo/api/coupon/api/CouponIssueApi.java
+++ src/main/java/com/example/demo/api/coupon/api/CouponIssueApi.java
@@ -5,6 +5,7 @@ import com.example.demo.api.common.app.NoDataException;
 import com.example.demo.api.coupon.app.CouponAlreadyExistException;
 import com.example.demo.api.coupon.app.CouponIssueRequest;
 import com.example.demo.api.coupon.app.CouponIssueService;
+import com.example.demo.api.coupon.domain.NoQuantityException;
 import com.example.demo.api.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +16,13 @@ public class CouponIssueApi {
     private final CouponIssueService couponIssueService;
 
     @PostMapping("/api/v1/coupons/{couponId}/issue")
-    public ApiResponse<Void> issueCoupon(@PathVariable Integer couponId, User user) {
+    public ApiResponse<?> issueCoupon(@PathVariable Integer couponId, User user) {
         try {
             couponIssueService.issueCoupon(new CouponIssueRequest(user.getId(), couponId));
-        } catch (NoDataException | CouponAlreadyExistException e) {
+        } catch (NoDataException e) {
             return ApiResponse.badRequest(e.getMessage());
+        } catch (NoQuantityException | CouponAlreadyExistException e){
+            return ApiResponse.ok(e.getMessage());
         }
 
         return ApiResponse.ok();
--- src/main/java/com/example/demo/api/coupon/domain/NoQuantityException.java
+++ src/main/java/com/example/demo/api/coupon/domain/NoQuantityException.java
@@ -1,4 +1,7 @@
 package com.example.demo.api.coupon.domain;
 
 public class NoQuantityException extends RuntimeException {
+    public NoQuantityException() {
+        super("쿠폰 수량이 부족합니다");
+    }
 }
